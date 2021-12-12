76d2f36e4fea96fec70ac35d6e843e5ed311177e
Leonardo <leonardo@mac.local>
2021-12-12 16:24:26 -0500

    untracked changes

--- WorldFlags/WorldFlags/FlagViewController.swift
+++ WorldFlags/WorldFlags/FlagViewController.swift
@@ -8,22 +8,49 @@
 import UIKit
 
 class FlagViewController: UIViewController {
+  @IBOutlet var flagView: UIImageView!
+  var flag: String?
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    loadFlagImage()
+    navigationItemSettings()
+    view.backgroundColor = UIColor.black
+  }
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.hidesBarsOnTap = true
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    navigationController?.hidesBarsOnTap = false
+  }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  private func loadFlagImage() {
+    if let flag = flag {
+      title = flag.uppercased()
+      flagView.image = UIImage(named: flag)
+      flagView.clipsToBounds = true
     }
-    
+  }
 
-    /*
-    // MARK: - Navigation
+  private func navigationItemSettings() {
+    navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    navigationItem.largeTitleDisplayMode = .never
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareFlag))
+  }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  @objc private func shareFlag() {
+    /// # Get the `flagView` image, that has already been set
+    guard let flagImage = flagView.image?.jpegData(compressionQuality: 0.8) else {
+      print("No image found")
+      return
     }
-    */
-
+    guard let countryName = flag else {
+      print("No contry name")
+      return
+    }
+    let ac = UIActivityViewController(activityItems: [flagImage, countryName], applicationActivities: nil)
+    present(ac, animated: true, completion: nil)
+  }
 }
--- WorldFlags/WorldFlags/TableViewController.swift
+++ WorldFlags/WorldFlags/TableViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-
+  var countries: [String] = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    setNavigationSettings()
+    /// # View's `background color`
+    view.backgroundColor = UIColor.black
   }
 
+  private func setNavigationSettings() {
+    title = "Flags"
+    navigationController?.navigationBar.prefersLargeTitles = true
+    /// # Change `navigation title color`
+    navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    navigationController?.navigationBar.barStyle = UIBarStyle.black
+  }
 
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return countries.count
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+    var content = cell.defaultContentConfiguration()
+    content.text = countries[indexPath.row].uppercased()
+    content.textProperties.color = UIColor.white.withAlphaComponent(0.8)
+    content.textProperties.font = UIFont.boldSystemFont(ofSize: 20)
+    cell.contentConfiguration = content
+    cell.backgroundColor = UIColor.black
+    /// # Cell's border color
+//    cell.layer.borderColor = UIColor.white.cgColor
+//    cell.layer.borderWidth = 0.4
+    /// # Background color when `cell is selected`
+    let bgColorView = UIView()
+    bgColorView.backgroundColor = UIColor.blue.withAlphaComponent(0.2)
+    cell.selectedBackgroundView = bgColorView
+    return cell
+  }
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print("row selected: \(indexPath.row)")
+    if let vc = storyboard?.instantiateViewController(withIdentifier: "Flag") as? FlagViewController {
+      vc.flag = countries[indexPath.row]
+      print("Cell selected: \(countries[indexPath.row])")
+      navigationController?.pushViewController(vc, animated: true)
+    }
+  }
 }
-
