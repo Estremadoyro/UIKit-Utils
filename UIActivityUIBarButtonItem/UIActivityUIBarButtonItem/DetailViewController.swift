eb134c5edf96ae6c03b7ae85d2c1358e90a34625
Leonardo <leonardo@mac.local>
2021-12-11 23:52:35 -0500

    storyboard changes...

--- UIActivityUIBarButtonItem/UIActivityUIBarButtonItem/DetailViewController.swift
+++ UIActivityUIBarButtonItem/UIActivityUIBarButtonItem/DetailViewController.swift
@@ -8,22 +8,66 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+  /// # Connects something to `InterfaceBuilder` aka StoryBoard
+  @IBOutlet var imageView: UIImageView!
+  var selectedImage: String?
+  var picturesList: [String]?
+  var currentPicture: Int?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    navigationItemSettings()
+    setImageView()
+  }
 
-        // Do any additional setup after loading the view.
+  private func setImageView() {
+    if let selectedImage = selectedImage {
+      imageView.image = UIImage(named: selectedImage)
     }
-    
+  }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  private func navigationItemSettings() {
+    if let picturesList = picturesList {
+      if let currentPicture = currentPicture {
+        title = "Picture \(currentPicture) of \(picturesList.count)"
+      } else {
+        title = "Picture X of \(picturesList.count)"
+      }
     }
-    */
+    navigationItem.largeTitleDisplayMode = .never
+    /// # Create a navigation-bar-item to share media
+    /// # `barButtonSystemItem` -> Item icon
+    /// # `target`              -> Where will the `action` method be located
+    /// # `action`              -> Method to call after tapped (Must be an `@objc` method) `Requires a Selector type`
+    /// # The `#selector` is a type in `@objc` that refers to the `name of an objc method`
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+    /// # `@IBAction` is automatically implied by `Objective-C`
+  }
 
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.hidesBarsOnTap = true
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    navigationController?.hidesBarsOnTap = false
+  }
+
+  /// # `@objc` -> Expose this method to `Objective-C`
+  @objc private func shareTapped() {
+    /// # Unwrap the image set in the `UIViewCell`
+    /// # `compressionQuality` goes from `0 to 1` (worst to best)
+    guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+      print("No image found")
+      return
+    }
+    /// # Create a `UIActivityViewController` to give option to share
+    let vc = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+    /// # Set the `barButtonItem` which will anchor the `popover`
+    /// # Whitout it, it won't run in iPad
+    vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+    /// # Show the `popover`
+    present(vc, animated: true)
+  }
 }
--- UIActivityUIBarButtonItem/UIActivityUIBarButtonItem/ViewController.swift
+++ UIActivityUIBarButtonItem/UIActivityUIBarButtonItem/ViewController.swift
@@ -7,9 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+  var pictures = [String]()
+  var pictureNames = [String]()
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    title = "StormViewer"
+    navigationController?.navigationBar.prefersLargeTitles = true
+    let fm = FileManager.default
+    let path = Bundle.main.resourcePath!
+    let items = try! fm.contentsOfDirectory(atPath: path)
+    for item in items {
+      if item.hasPrefix("nssl") {
+        pictures.append(item)
+        pictureNames.append(removeSuffix(pictureName: item))
+      }
+    }
+    pictures = sortItems()
+  }
+
+  private func removeSuffix(pictureName: String) -> String {
+    return pictureName.components(separatedBy: ".")[0]
+  }
+
+  private func sortItems() -> [String] {
+    return pictures.sorted(by: { $0 < $1 })
+  }
+
+  /// # Set table's # rows
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return pictures.count
+  }
+
+  /// # Set table's row contents
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+    var content = cell.defaultContentConfiguration()
+    content.textProperties.color = UIColor.blue
+    content.textProperties.font = .boldSystemFont(ofSize: 20)
+    content.text = pictureNames[indexPath.row]
+    cell.contentConfiguration = content
+    return cell
+  }
+
+  /// # Set action when cell is selected
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+      vc.selectedImage = pictures[indexPath.row]
+      vc.picturesList = pictures
+      vc.currentPicture = indexPath.row + 1
+      navigationController?.pushViewController(vc, animated: true)
+    }
   }
 }
