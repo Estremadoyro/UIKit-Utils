17e2d95add9c55f5392cc1136c6d5db7af256c38
Leonardo <leonardo@mac.local>
2022-02-18 19:11:21 -0500

    milestone project 19-21, NotesApp clone

---
+++ NotesClone/NotesClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesClone
+//
+//  Created by Leonardo  on 18/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
