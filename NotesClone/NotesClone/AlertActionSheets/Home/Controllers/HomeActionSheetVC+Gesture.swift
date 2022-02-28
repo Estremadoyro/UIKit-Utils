49e2c5e44c243413705da9a26c6e6da3dc7ac93d
Leonardo <leonardo@mac.local>
2022-02-28 00:32:29 -0500

    alert action sheet dragging up and down working, doing some refactor, using top anchor instead of height

--- NotesClone/NotesClone/AlertActionSheets/Home/Controllers/HomeActionSheetVC+Gesture.swift
+++ NotesClone/NotesClone/AlertActionSheets/Home/Controllers/HomeActionSheetVC+Gesture.swift
@@ -11,37 +11,50 @@ extension HomeActionSheetVC {
   @IBAction func didPanAction(_ gesture: UIPanGestureRecognizer) {
     let dragTranslation = gesture.translation(in: self.view)
     let actionSheetHeight = homeActionSheetView.frame.size.height
-    let masterViewHeight = view.frame.size.height
+    let masterViewHeight = view.frame.size.height 
     switch gesture.state {
       case .began:
         print("")
       case .changed:
 //        print("pan changed")
-        if alertIsFullHeight.isActive {
-          alertIsFullHeight.constant = -dragTranslation.y
+
+        if actionSheetHeight <= masterViewHeight {
+          print("actionSheetHeight: \(actionSheetHeight)")
+          if alertIsFullHeight.isActive {
+            alertIsFullHeight.constant = -dragTranslation.y
+            print("full constant: \(alertIsDisplayed.constant)")
+          } else {
+            alertIsDisplayed.constant = -dragTranslation.y
+            print("mid constant: \(alertIsDisplayed.constant)")
+          }
         } else {
-          alertIsDisplayed.constant = -dragTranslation.y
-          print("constant: \(alertIsDisplayed.constant)")
+          print("alertIsFullHeight constant: \(alertIsFullHeight.constant)")
+          print("alertIsDisplayed constant: \(alertIsDisplayed.constant)")
+          print("max dragging reached")
         }
 
       case .ended:
         print("pan ended")
         print("Action height: \(actionSheetHeight)")
+        print("Master height: \(masterViewHeight)")
         print("1/4 frame height: \(masterViewHeight / 4)")
         if actionSheetHeight <= (masterViewHeight / 4) {
           print("hide action")
           dismissActionSheetWithAnimation()
         } else if (actionSheetHeight / 2) + (-dragTranslation.y) > (masterViewHeight * 3 / 5) {
           print("full screen action")
+          alertIsFullHeight.constant = 0
           alertIsFullHeight.isActive = true
           alertIsDisplayed.isActive = false
           alertIsHidden.isActive = false
+          print("FULL SCREEN HEIGHT: \(homeActionSheetView.frame.height)")
         } else {
           print("else go back to original")
           alertIsDisplayed.isActive = true
           alertIsFullHeight.isActive = false
           alertIsHidden.isActive = false
           alertIsDisplayed.constant = 0
+          alertIsFullHeight.constant = 0
         }
 
       case .cancelled:
