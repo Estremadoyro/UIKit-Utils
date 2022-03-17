dff8876bcf77de939e8a6862671d33e7a73e9220
Leonardo <lmestremadoyro@gmail.com>
2022-03-17 17:47:00 -0500

    project26 setup

---
+++ Project26-CoreMotion-MarbleMaze/Project26-CoreMotion-MarbleMaze/GameScene.swift
@@ -0,0 +1,12 @@
+//
+//  GameScene.swift
+//  Project26-CoreMotion-MarbleMaze
+//
+//  Created by Leonardo  on 17/03/22.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+  override func didMove(to view: SKView) {}
+}
